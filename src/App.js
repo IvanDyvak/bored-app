b10ee4ac7b8bde1dc308ab7e2e0e65435daef62a
Ivan <you@example.com>
2022-11-08 00:12:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="images/logo.png" className="logo-img" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
