ade445161e1cf8c858a4d52aa9448ba6c65c55e8
Ivan <you@example.com>
2022-11-08 14:17:39 +0200

    project development

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         data,
         isDataLoading,
         selected,
+        setSelected,
         handleChange,
         handleSubmit,
     } = useAppData();
@@ -15,25 +16,32 @@ function App() {
 
     useEffect(() => {
         handleSubmit();
-    }, [selected]);
+    }, [setSelected]);
 
+    const {activity, type, price} = data;
 
-
+    //
+    // if (!data.length) return <div style={{textAlign: "center", marginTop: "50px", fontWeight: "600"}}>Sorry, there's no
+    //     info</div>
 
     return (
     <div className="App">
-      <header className="App-header">
-        <img src="images/logo.png" className="logo-img" alt="logo" />
-      </header>
-        <form
-            onSubmit={(e) => {handleSubmit(); e.preventDefault();}}
-            // onSubmit={handleSubmit}
-        >
-            <label htmlFor="participants">Select a number of participants: </label>
-            <select  id="participants"
+        <div className="header-wrap">
+            <header className="header">
+                <img src="images/logo.png" className="logo-img" alt="logo" />
+            </header>
+        </div>
+        <div className="container">
+            <div className="description">
+                <h4>Feeling bored and don't know what to do? Discover tons of activities in the blink of an eye. Find something suitable for yourself or even get your friends involved. No restrictions. Click the button below as many times as you wish until you run into something worth for you.</h4>
+            </div>
+            <form
+                onSubmit={(e) => {handleSubmit(); e.preventDefault();}}>
+                <label htmlFor="participants">Select a number of participants: </label>
+                <select  id="participants"
                          name="participants"
                          defaultValue={selected}
-                        onChange={handleChange}>
+                         onChange={handleChange}>
                     <option value="0">0</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -41,18 +49,19 @@ function App() {
                     <option value="4">4</option>
                     <option value="5">5</option>
                 </select>
-            <input type="submit" value="Get More" className="init-btn"/>
-        </form>
-        {/*{data.map(({activity}) =>*/}
-        {/*    <div className="offer" key={Math.random()}>*/}
-        {/*        <p>{activity}</p>*/}
-        {/*    </div>*/}
-        {/*)}*/}
-        <div className="offer" key={Math.random()}>
-                <p>{data.activity}</p>
+                <input style={{ backgroundImage: `url("images/btn-bg.jpg")` }} type="submit" value={`${isDataLoading ? 'Loading...' : 'Check Now'}`} className={`${isDataLoading ? 'init-btn loading' : 'init-btn'}`} />
+            </form>
+            <div className="offer">
+                { selected === 0 ?
+                    null :
+                    <>
+                        <h2 className="activity">{activity}</h2>
+                        <p className="type">Type of activity: {type}</p>
+                        <p className="price">Price: {price}$</p>
+                    </>
+                }
             </div>
-
-
+        </div>
     </div>
   );
 }
--- src/context/user-hooks.js
+++ src/context/user-hooks.js
@@ -9,7 +9,7 @@ export const AppProvider = ({ children }) => {
     const [ error, setError ] = useState(null);
     const [ isDataLoading, setDataLoading ] = useState(false);
     const [ data, setData ] = useState([]);
-    const [selected, setSelected] = useState(0);
+    const [ selected, setSelected ] = useState(0);
 
 
     const handleChange = (e) => {
@@ -18,22 +18,17 @@ export const AppProvider = ({ children }) => {
 
    const handleSubmit = () => {
        setDataLoading(true);
-       getData(`http://www.boredapi.com/api/activity?participants=${selected}`)
+       getData(`https://www.boredapi.com/api/activity?participants=${selected}`)
            .then(res => {
                const { message, code } = res;
                if (code !== '200' && message) throw Error(message);
                setError(null);
                setData(res);
-               console.log(data);
-
            })
            .catch(setError)
            .finally(() => setDataLoading(false));
    }
-    // if (isDataLoading) return <div style={{textAlign: "center", marginTop: "50px", fontWeight: "600"}}>Loading...</div>
-    //
-    // if (!data.length) return <div style={{textAlign: "center", marginTop: "50px", fontWeight: "600"}}>Sorry, there's no
-    //     info</div>
+
   return (
     <AppContext.Provider
       value={{
